f019eafa18ed3b10310b50146245260482662465
sue-guo <guo00135@algonquinlive.com>
2024-07-28 15:50:16 -0400

    add Chinese locale

--- lib/reservation/Resercvation.dart
+++ lib/reservation/Resercvation.dart
@@ -1,20 +1,34 @@
 
 import 'package:floor/floor.dart';
 
+/// Represents an Reservation entity.
+///
+/// This class is used to define the structure of an Reservation record in the database.
+/// It contains information about the Reservation's ID, customerId, flightId, and date of departure
+///
+/// @author: Hongxiu Guo
 @entity
 class Reservation{
-
+  /// A static variable to keep track of the latest ID.
   static int ID = 1;
 
+  /// The primary key of the reservation table.
   @primaryKey // id is the primary key of the table
   final int id;
 
+  /// The ID of the customer from customer database
   final int customerId;
 
+  /// The ID of the flight from flight database
   final String flightId;
 
+  /// The departure date of the flight.
   final String departureTime;
 
+  /// Constructor for creating a `Reservation` object.
+  ///
+  /// Takes in an ID, customer ID, flight ID, and departure time.
+  /// Updates the static `ID` variable to ensure unique IDs.
   Reservation(this.id, this.customerId, this.flightId, this.departureTime){
     if(id >= ID) ID = id + 1;
   }
--- lib/reservation/ReservationDatabase.dart
+++ lib/reservation/ReservationDatabase.dart
@@ -4,9 +4,18 @@ import 'package:floor/floor.dart';
 import 'dart:async';
 import 'package:sqflite/sqflite.dart' as sqflite;
 part 'ReservationDatabase.g.dart';
-
+/// The [ReservationDatabase] class represents the Room database for managing [Reservation] entities.
+///
+/// This abstract class defines the database schema and provides access to [ReservationDao],
+/// which contains methods to interact with [Reservation] entities in the database.
+///
+/// @author: Hongxiu Guo
 @Database(version: 1, entities: [Reservation])
 abstract class ReservationDatabase  extends FloorDatabase{
+  /// Provides access to the [ReservationDao] for performing CRUD operations on [Reservation] entities.
+  ///
+  /// Use this getter to obtain an instance of [ReservationDao], which allows you to query,
+  /// insert, update, and delete reservation.
   ReservationDao get reservationDao;
 
 }
