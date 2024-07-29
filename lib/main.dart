1ef5e3f848ceed7f72b0e0a490fb35399b989d79
yaoyi <yyaome@hotmail.com>
2024-07-28 20:08:44 -0400

    Add Chinese locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,77 @@
 import 'package:flutter/material.dart';
 
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'AppLocalizations.dart';
 import 'flight/flight_list_page.dart';
-import 'Airplane/AirplanePage.dart';
 
 
+/// The main entry point of the application.
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+/// The root widget of the application.
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+  @override
+  _MyAppState createState() {
+    return _MyAppState();
+  }
+  /// Changes the locale of the app.
+  ///
+  /// Takes in a [BuildContext] and a [Locale] object. Finds the ancestor state
+  /// of type [_MyAppState] and calls the [changeLanguage] method.
+  static void setLocale(BuildContext context, Locale newLocale) async {
+    _MyAppState? state = context.findAncestorStateOfType<_MyAppState>();
+    state?.changeLanguage(newLocale);
+  }
+
+}
+/// The state for the [MyApp] widget.
+class _MyAppState extends State<MyApp>
+{
+  /// The current locale of the app. Default is English (Canada).
+  var _locale = Locale("en", "CA"); //default is english from Canada
+
+  /// Changes the language of the app.
+  ///
+  /// Takes in a [Locale] object and sets the state to the new language.
+  void changeLanguage(Locale newLanguage)
+  {
+    setState(() {
+      _locale = newLanguage; //set app to new language, and redraw
+    });
+
+  }
+
+  /// Builds the widget tree.
+  ///
+  /// This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      supportedLocales:  const<Locale> [
+        Locale("en", "CA"),
+        Locale("zh", "CH"),//country doesn't matter in this case
+      ] ,
+
+      localizationsDelegates: const[
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      locale: _locale, //default is "en", "CA" from above
+
       title: 'Flutter Demo',
 
       //routers
       initialRoute: '/',
       routes: {
-        '/': (context) => const MyHomePage(title: 'Home Page'),
+        '/': (context) => const MyHomePage(title: 'Home Page'), //Same as above =>
         '/FlightPage': (context) { return const FlightPage();},
-      },
-
-      theme: ThemeData(
-        // This is the theme of your application.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-        '/AirplanePage': (context) { return  AirplanePage(); }, //Same as above =>
       },
 
       theme: ThemeData(
@@ -43,19 +83,11 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+/// The home page of the application.
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  /// The title of the home page.
   final String title;
 
   @override
@@ -74,18 +106,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+              onPressed: (){Navigator.pushNamed(context,"/CustomerPage" );},
+              child:const Text("Customer List", style: TextStyle(color: Colors.blue)),
+            ),
             ElevatedButton(
               onPressed: (){Navigator.pushNamed(context,"/AirplanePage" );},
               child:const Text("Airplane List", style: TextStyle(color: Colors.blue)),
@@ -94,6 +127,10 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: (){Navigator.pushNamed(context,"/FlightPage" );},
               child:const Text("Flight List", style: TextStyle(color: Colors.blue)),
             ),
+            ElevatedButton(
+              onPressed: (){Navigator.pushNamed(context,"/ReservationPage" );},
+              child:const Text("Reservation List", style: TextStyle(color: Colors.blue)),
+            )
 
           ],
         ),
